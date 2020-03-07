14c015c2e16e719e68d422deae77faf20130a4cb
apiavis <agrant2589@gmail.com>
2020-03-07 08:09:19 -1000

    execute SQL query to create table in database

--- Connect.php
+++ Connect.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once('config.php');
 
 class Connect { 
@@ -22,5 +21,4 @@ class Connect {
 $sqlStatement = "CREATE TABLE `employee_portal_website`.`tblUser` ( `id` INT(10) NOT NULL AUTO_INCREMENT , `email` VARCHAR(50) NULL , `password` VARCHAR(20) NOT NULL , `firstName` VARCHAR(50) NULL , `lastName` VARCHAR(50) NOT NULL , `address` VARCHAR(250) NULL DEFAULT NULL , `phone` VARCHAR(13) NULL DEFAULT NULL , `salary` INT(10) NULL DEFAULT NULL , `SSN` INT(9) NULL DEFAULT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB";
 $newConnection = new Connect();
 $newConnection->executeQuery($newConnection->connection,$sqlStatement);
-
 ?>
