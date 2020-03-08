d68d01cb222dc2aa7f0a0b9ed0cee40945b02020
apiavis <agrant2589@gmail.com>
2020-03-08 09:38:34 -1000

    add loop to display options in state dropdown

--- registration.php
+++ registration.php
@@ -59,7 +59,14 @@
                     <label for="inputState">State</label>
                     <select id="inputState" class="form-control">
                         <option selected>Choose...</option>
-                        <option>...</option>
+                        <?php
+                        $states = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"];
+                        $stateLength = count($states);
+                        for ($i=0; $i<$stateLength; $i++) {
+                            echo "<option>$states[$i]</option>";
+                        }
+                        ?>
+                        <!-- <option>...</option> -->
                     </select>
                 </div>
                 <div class="form-group col-md-2" id="no-padding-right">
